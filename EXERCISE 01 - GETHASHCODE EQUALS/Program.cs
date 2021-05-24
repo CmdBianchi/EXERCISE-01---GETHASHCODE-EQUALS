73c97e59a95ea595643c8726ed9d426fc2268bc8
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-24 16:49:59 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - GETHASHCODE EQUALS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EXERCISE_01___GETHASHCODE_EQUALS {
+    //-----> START
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+
+            string a = "Maria";
+            string b = "Alex";
+            string c = "Alex";
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
